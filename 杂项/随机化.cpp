aad9ccaed0e1086427d886cef2e29f32beb50e7c
Isdiaoman <3086949709@qq.com>
2026-06-05 11:23:07 +0800

    添加了随机化mt19937及对应的random模板函数

--- 杂项/随机化.cpp
+++ 杂项/随机化.cpp
@@ -14,3 +14,10 @@ static i64 random(i64 a, i64 b) {
     i64 x = shift((u64)rand(), (u64)rand());
     return  (x % (i64)(b - a + 1) + (b - a + 1)) % (b - a + 1) + a;
 }
+
+
+mt19937_64 rg(random_device{}());//随机数生成器
+template<typename T>
+T random(T _L, T _R) {//获得闭区间 [L,R] 的随机数
+    return rg() % (_R - _L + 1) + _L;
+}
